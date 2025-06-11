929b7f45bcb1e31e568fef528a6a1d1bf5a1dec8
amalfranci <amalfrancis744@gmail.com>
2025-06-11 18:06:11 +0530

    feat: add MovieCard and TrendingCard components, integrate trending movies fetching, and update .env variable for API key

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -2,5 +2,11 @@ import { fetchMovies } from "./api";
 
 export const getTrendingMovies = async () => {
     const movies = await fetchMovies({ query: "" });
-    return movies.slice(0, 5); // Return top 5 trending movies
+    return movies.slice(0, 5).map(movie => ({
+        searchTerm: movie.title,
+        movie_id: movie.id,
+        title: movie.title,
+        count: movie.vote_count,
+        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+    }));
 }; 
